fb897f20ee6b3a0c7779b0958d54475ca6144ea9
John Doe <kubanam1995@gmail.com>
2023-05-19 11:22:55 +0200

    Ul listener was added

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -1,35 +1,42 @@
 const db = firebase.firestore();
-const dataRef = db.collection("data").doc("time");
+const dataRef = db.collection("data").doc(); //zmien nazwe
+const dataCollectionRef = db.collection("data");
 
-export function pushData() {
-    dataRef.set()
-}
 
+//getdata trzeba zmienic na on snapshot
 export async function getData() {
     try {
-        let data;
-        return data = await dataRef.get()
-    } catch (err) {
-        return console.log(err)
+        const querySnapshot = await dataCollectionRef.get();
+        const arrObj = querySnapshot.docs.map(doc => doc.data());
+        return arrObj;
+    } catch (error) {
+        console.error("Wystąpił błąd podczas pobierania danych:", error);
+        return [];
     }
+}
 
-};
 
-export const changeToArr = async (data) => {
-    let documents = await data;
-    let arr = Object.values(documents.data())
-    return arr;
+export const changeToSimpleArr = async (data) => {
+    let arrVal = [];
+    await data.then((dataArr) => {
+        dataArr.map(dataArr => {
+            for (const key in dataArr) {
+                const value = dataArr[key];
+                arrVal.push(value)
+                console.log(arrVal);
+              }
+        })
+    })
+ return arrVal;
 }
 
 export async function setData(data) {
-   try{
-    await dataRef.set(data)
-    console.log('dodales dane')
-   } catch (err) {
-    console.log(err)
-   }
-
-
+    try {
+        await dataRef.set(data)
+        console.log('dodales dane')
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 // musisz przeanalizowac jak powinna wygladac relacja miedzy baza danych, a strona www.
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,27 +1,25 @@
 import {
     getData,
-    changeToArr,
+    changeToSimpleArr,
     setData
 } from "./database.js";
 
-const addBtn = document.querySelector('.addBtn')
-const foodInput = document.querySelector('input[name="inputFood"]')
+const addBtn = document.querySelector('.addBtn');
+const foodInput = document.querySelector('input[name="inputFood"]');
+const ulEle = document.querySelector('section ul');
 
 addBtn.addEventListener('click', e => {
     e.preventDefault();
-
-    console.log(foodInput.value)
     setData({pasza: foodInput.value})
-    // foodInput.value = ''
+    foodInput.value = ''
     // w przyszlosci mozesz zrobisz .reset() na formularzu, wyczyscisz all inputy
 });
 
 
 const addToList = (values) => {
-    let ul = document.querySelector('section ul')
     values.then(arr => {
             for (let i = 0; i < arr.length; i++) {
-                ul.innerHTML += `<li>${arr[i]}</li>`;
+                ulEle.innerHTML += `<li>${arr[i]}</li>`;
             }
         })
         .catch(err => {
@@ -29,4 +27,14 @@ const addToList = (values) => {
         })
 }
 
-addToList(changeToArr(getData()))
+addToList(changeToSimpleArr(getData()))
+
+ulEle.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(e.target)
+    if(e.target.tagName === 'LI'){
+        console.log('jebac prace')
+        e.target.remove();
+    }
+    // console.log(e.)
+})
